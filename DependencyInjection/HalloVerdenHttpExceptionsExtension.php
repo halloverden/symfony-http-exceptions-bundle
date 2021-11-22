f90852039e47808a3ddf77ff519b6bff9fc63611
Tom Roar Furunes <tom.roar@halloverden.no>
2021-11-22 15:51:58 +0100

    ADD: make use_flatten_exception a configurable option.

--- DependencyInjection/HalloVerdenHttpExceptionsExtension.php
+++ DependencyInjection/HalloVerdenHttpExceptionsExtension.php
@@ -4,20 +4,28 @@
 namespace HalloVerden\HttpExceptionsBundle\DependencyInjection;
 
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use HalloVerden\HttpExceptionsBundle\Interfaces\Services\ExceptionLogServiceInterface;
+use HalloVerden\HttpExceptionsBundle\Services\ExceptionLogService;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\ConfigurableExtension;
 
-class HalloVerdenHttpExceptionsExtension extends Extension {
+class HalloVerdenHttpExceptionsExtension extends ConfigurableExtension {
 
   /**
    * @inheritDoc
    * @throws \Exception
    */
-  public function load(array $configs, ContainerBuilder $container) {
+  protected function loadInternal(array $mergedConfig, ContainerBuilder $container) {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
     $loader->load('services.yaml');
-  }
 
+    $exceptionLog = $container->getDefinition(ExceptionLogServiceInterface::class);
+    if ($exceptionLog->getClass() === ExceptionLogService::class) {
+      $exceptionLog->addArgument([
+        '$useFlattenException' => $mergedConfig['exception_log']['use_flatten_exception']
+      ]);
+    }
+  }
 }
